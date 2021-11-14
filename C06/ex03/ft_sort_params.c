531b86ad6618097c90205b75e14e2620e977aec2
MaelMD <43723938+MaelMD@users.noreply.github.com>
2021-11-14 20:31:13 +0100

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmohamme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/03 18:02:27 by mmohamme          #+#    #+#             */
+/*   Updated: 2021/10/11 20:32:50 by mmohamme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	to_write(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i + 1], av[i]) < 0)
+		{
+			av[ac + 1] = av[i + 1];
+			av[i + 1] = av[i];
+			av[i] = av[ac + 1];
+			i = 0;
+		}
+		i++;
+	}
+	to_write(ac, av);
+	return (0);
+}
